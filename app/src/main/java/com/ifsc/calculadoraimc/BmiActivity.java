fe504e56f922802331b6638e9cc27f7f4ba300c3
Bernardo Mesko <bemesko@gmail.com>
2021-02-28 18:12:36 -0300

    Projeto 2 Concluído

--- app/src/main/java/com/ifsc/calculadoraimc/BmiActivity.java
+++ app/src/main/java/com/ifsc/calculadoraimc/BmiActivity.java
@@ -3,12 +3,43 @@ package com.ifsc.calculadoraimc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class BmiActivity extends AppCompatActivity {
 
+    String name;
+    Double weight, height;
+    TextView txtName, txtWeight, txtHeight, txtBMI;
+    DecimalFormat decimalFormat = new DecimalFormat("##,###,###,##0.00");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+
+        Bundle bundle = getIntent().getExtras();
+        name = bundle.getString("name");
+        height = bundle.getDouble("height");
+        weight = bundle.getDouble("weight");
+
+        txtName = findViewById(R.id.txtNameValue);
+        txtHeight = findViewById(R.id.txtHeightValue);
+        txtWeight = findViewById(R.id.txtWeightValue);
+        txtBMI = findViewById(R.id.txtBMIValue);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        txtName.setText(name);
+        txtHeight.setText(height.toString());
+        txtWeight.setText(weight.toString());
+        txtBMI.setText(decimalFormat.format(CalculateBMI(weight, height)));
+    }
+
+    public Double CalculateBMI(Double weight, Double height){
+        Double bmi = weight/(height*height);
+        return bmi;
     }
 }
--- app/src/main/java/com/ifsc/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/ifsc/calculadoraimc/MainActivity.java
@@ -2,13 +2,35 @@ package com.ifsc.calculadoraimc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edTxtName, edTxtHeight, edTxtWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edTxtName = findViewById(R.id.editTextName);
+        edTxtHeight = findViewById(R.id.editTextHeight);
+        edTxtWeight = findViewById(R.id.editTextWeight);
+    }
+
+    public void calculateBMI(View view){
+        Intent intent = new Intent(this, BmiActivity.class);
+
+        String name = edTxtName.getText().toString();
+        Double weight = Double.parseDouble(edTxtWeight.getText().toString());
+        Double height = Double.parseDouble(edTxtHeight.getText().toString());
+
+        intent.putExtra("name", name);
+        intent.putExtra("height", height);
+        intent.putExtra("weight", weight);
+
+        startActivity(intent);
     }
 }
